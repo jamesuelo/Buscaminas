078bfd01365f1de5333ba59d5a765761a69c1f3d
jamesuelo <jaime_mtz99@hotmail.com>
2022-04-29 00:24:32 +0200

    full project

---
+++ Buscaminas/Assets/Scripts/GridHelper.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHelper : MonoBehaviour
+{
+    public float mineWeight = 0.1f;
+    public static int w = 23;
+    public static int h = 19;
+
+    public static cell[,] cells = new cell[w, h];
+    // Start is called before the first frame update
+    public static void UncoverAllMines(){
+        foreach (cell c in cells)
+        {
+            if(c.hasMine){
+                c.loadTexture(0);
+            }
+        }
+    }
+    public static bool HasMineAt(int x, int y){
+        if(x>=0 && y >=0 && x<w && y<h){
+            cell c = cells[x,y];
+            return c.hasMine;
+        }
+        else{
+            return false;
+        }
+    }
+    public static int countAdjMines(int x, int y){
+        int count = 0;
+        if(HasMineAt(x-1,y-1)) count++;
+        if(HasMineAt(x-1,y)) count++;
+        if(HasMineAt(x,y-1)) count++;
+        if(HasMineAt(x+1,y-1)) count++;
+        if(HasMineAt(x-1,y+1)) count++;
+        if(HasMineAt(x+1,y)) count++;
+        if(HasMineAt(x,y+1)) count++;
+        if(HasMineAt(x+1,y+1)) count++;
+        return count;
+    }
+
+
+    public static void FloodFill(int x, int y, bool [,] visited){
+        if(x>=0 && y >=0 && x<w && y<h){
+        
+        if(visited[x,y])
+        {
+            return;
+        }
+
+        int count = countAdjMines(x,y);
+
+        cells[x,y].loadTexture(count);
+        if(count > 0) return;
+
+        visited[x,y] = true;
+        FloodFill(x,y+1, visited);
+        FloodFill(x+1,y, visited);
+        FloodFill(x,y-1, visited);
+        FloodFill(x-1,y, visited);
+    }
+    }
+
+        public static bool GameEnd(){
+        foreach (cell c in cells)
+        {
+            if(c.isCovered() && !c.hasMine){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Buscaminas/Assets/Scripts/cell.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class cell : MonoBehaviour
+{
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+    public bool hasMine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GridHelper helper = GameObject.Find("Panel").GetComponent<GridHelper>();
+        hasMine = (Random.value < helper.mineWeight);
+        
+        int x  = (int)this.transform.position.x;
+        int y = (int)this.transform.parent.transform.position.y;
+        GridHelper.cells[x,y] = this;
+    }
+
+    public void loadTexture(int adjacent)
+    {
+        if(hasMine)
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacent];
+        }
+    }
+
+
+public bool isCovered(){
+    return GetComponent<SpriteRenderer>().sprite.texture.name == "Casilla";
+
+}
+
+private void OnMouseUpAsButton(){
+
+    if(hasMine){
+
+        GridHelper.UncoverAllMines();
+        Invoke("ReturnTo", 3.0f);
+    }
+    else
+    {
+        int x = (int) this.transform.position.x;
+        int y = (int) this.transform.parent.transform.position.y;
+        loadTexture(GridHelper.countAdjMines(x,y));
+        GridHelper.FloodFill(x,y,new bool[GridHelper.w,GridHelper.h]);
+
+        if(GridHelper.GameEnd()){
+            Debug.Log("Fin de la Partida, Ganaste pendejo");
+            Invoke("ReturnTo", 3.0f);
+        }
+
+    }
+}
+
+    private void ReturnTo(){
+        SceneManager.LoadScene("Buscaminas");
+    }
+
+}
